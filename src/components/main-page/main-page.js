73287a450a0710ce8877326cb549a1af688c6d16
JS <jasonswonABK@gmail.com>
2019-09-21 00:06:56 +0300

    init

--- src/components/main-page/main-page.js
+++ src/components/main-page/main-page.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import AboutMe from "../about-me/about-me";
 import Massage from "../massage/massage";
 import FitBall from "../fitball/fitball";
-import CertificatesList from "../certificates-list/certificates-list";
 import GoTop from "../go-top/go-top";
 import FirstScreen from "../first-screen/first-screen";
 import Feedback from "../feedback/feedback";
-import { Route } from 'react-router-dom';
 
 export default class MainPage extends Component{
     state = {
@@ -44,8 +42,6 @@ export default class MainPage extends Component{
 
 
     render() {
-        let certificates = this.state.canSee ? <CertificatesList exitClick={this.buttonClick} /> : undefined;
-
         return(
             <div onScroll={this.scrolling}>
                 {this.state.goUp ? <GoTop /> : undefined}
